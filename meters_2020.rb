591b14f8dff7b7118ceb880158fa15b3ba05c088
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-05-23 11:14:59 +0400

    Add help-strings
    Добавили пояснения.

--- meters_2020.rb
+++ meters_2020.rb
@@ -5,13 +5,13 @@ def info_20_05
 
 	@date = '2020-05-23'
 
-	@meter_electricity = 27_800
+	@meter_electricity = 27_800    #  Электроэнергия
 
-	@meter_cook_room_cold =   290
-	@meter_cook_room_hot_ =   199
-	@meter_both_room_cold = 1_105
-	@meter_both_room_hot_ =   451
+	@meter_cook_room_cold =   290  #  Кухня хорлная ХВС
+	@meter_cook_room_hot_ =   199  #  Кухня горячая ГВС
+	@meter_both_room_cold = 1_105  # Ванная хорлная ХВС
+	@meter_both_room_hot_ =   451  # Ванная горячая ГВС
 
 	put_info
-	
+	add_info
 end
