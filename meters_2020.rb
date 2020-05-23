245dd07fa0a47f2f4166e5840fcce9d0f35ac255
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-05-23 09:29:10 +0400

    Add new file to save info for 2020 year
    Добавим первую процедуру с показаниями счётчиков.

---
+++ meters_2020.rb
@@ -0,0 +1,15 @@
+# It is file for save meters-info forever
+#
+
+def info_20_05
+
+	date = '2020-05-23'
+
+	meter_electricity = 27_800
+
+	meter_cook_room_cold =   290
+	meter_cook_room_hot_ =   199
+	meter_cook_room_cold = 1_105
+	meter_cook_room_hot_ =   451
+	 
+end
